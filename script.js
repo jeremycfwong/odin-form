795b1634eb16e789cde82d0b74c1003411b117c4
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2023-01-11 00:00:46 +0800

    Add form validation
    As Title

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var emailElement = document.getElementById('email');
+
+emailElement.addEventListener('input', (_) => {
+    if (emailElement.validity.typeMismatch) {
+        emailElement.setCustomValidity('please enter a valid email');
+        emailElement.classList.add('wrong');
+    } else {
+        emailElement.setCustomValidity('');
+        emailElement.classList.remove('wrong');
+    };
+});
+
+// var countryElement = document.getElementById('country')
+
+var zipcodeElement = document.getElementById('zipcode');
+zipcodeElement.pattern = `\\d{5,5}`;
+
+
+function validateMyForm() {
+    var passwordElement = document.getElementById('password');
+    var cfmPasswordElement = document.getElementById('cfm-password');
+
+    if (passwordElement.value == cfmPasswordElement.value 
+        && passwordElement.value != ''){
+            alert('high five');
+    };
+};
